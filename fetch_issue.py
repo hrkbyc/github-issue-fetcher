fded21cede7fcdb35b83abf790e5d53ca479789c
hrkbyc <hrkbyc@gmail.com>
2023-06-29 16:38:42 +0900

    update: 複数のリポジトリから取得出来るように変更

--- fetch_issue.py
+++ fetch_issue.py
@@ -9,27 +9,30 @@ import requests
 load_dotenv()
 
 access_token = os.getenv('ACCESS_TOKEN')
-repository = os.getenv('REPOSITORY')
+repositories_str = os.getenv('REPOSITORIES')
+repositories = repositories_str.split(",")
 github_user = os.getenv('GITHUB_USER')
 
 headers = {'Authorization': 'token ' + access_token}
-url = 'https://api.github.com/repos/' + github_user + '/' + repository + '/issues'
+url_base = 'https://api.github.com/repos/' + github_user + '/'
 params = {"per_page": 100, "state": "closed", "direction": "asc"}
 
 with open('issues.csv', 'w') as f:
   writer = csv.writer(f)
   writer.writerow(['repository', '#', 'title', 'closed_at', 'label', 'assignee'])
-  for number in range(20):
-    params['page'] = number + 1
-    response = requests.get(url, params=params, headers=headers)
-    issues = json.loads(response.content)
-    if len(issues) > 0:
-      for issue in issues:
-        if issue['assignees'] and issue['labels'] and issue['labels'][0]['name'] != 'Break':
-          # loginのみでストを作る
-          assignee_names = [assignee.get('login') for assignee in issue['assignees']]
-          label_name = issue['labels'][0]['name']
-          closed_at_utc = datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
-          closed_at_jst = timezone('Asia/Tokyo').localize(closed_at_utc)
-          closed_at = closed_at_jst.strftime('%Y-%m-%d')
-          writer.writerow([repository, issue['number'], issue['title'], closed_at, label_name, ",".join(assignee_names)])
+  for repository in repositories:
+    for number in range(20):
+      params['page'] = number + 1
+      url = url_base + repository + '/issues'
+      response = requests.get(url, params=params, headers=headers)
+      issues = json.loads(response.content)
+      if len(issues) > 0:
+        for issue in issues:
+          if issue['assignees'] and issue['labels'] and issue['labels'][0]['name'] != 'Break':
+            # loginのみでストを作る
+            assignee_names = [assignee.get('login') for assignee in issue['assignees']]
+            label_name = issue['labels'][0]['name']
+            closed_at_utc = datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
+            closed_at_jst = timezone('Asia/Tokyo').localize(closed_at_utc)
+            closed_at = closed_at_jst.strftime('%Y-%m-%d')
+            writer.writerow([repository, issue['number'], issue['title'], closed_at, label_name, ",".join(assignee_names)])
