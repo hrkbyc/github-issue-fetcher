5f2c0a574cb1c35787ca6dde9e28c0a26d361029
hrkbyc <hrkbyc@gmail.com>
2023-06-29 17:05:50 +0900

    update: 出力されるファイル名、保存先を調整

--- fetch_issue.py
+++ fetch_issue.py
@@ -13,11 +13,23 @@ repositories_str = os.getenv('REPOSITORIES')
 repositories = repositories_str.split(",")
 github_user = os.getenv('GITHUB_USER')
 
+# フォルダが存在しない場合に作成する
+output_folder = "output"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+def generate_filename_with_date():
+  today = datetime.today()
+  filename = today.strftime("issue-%Y%m%d%H%M%S.txt")
+  return filename
+
 headers = {'Authorization': 'token ' + access_token}
 url_base = 'https://api.github.com/repos/' + github_user + '/'
 params = {"per_page": 100, "state": "closed", "direction": "asc"}
+file_name = generate_filename_with_date()
+file_path = os.path.join(output_folder, file_name)
 
-with open('issues.csv', 'w') as f:
+with open(file_path, 'w') as f:
   writer = csv.writer(f)
   writer.writerow(['repository', '#', 'title', 'closed_at', 'label', 'assignee'])
   for repository in repositories:
